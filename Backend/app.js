dd4ee6930baa22a6625407e00a5a4beaecc3589d
Jungflorian <jungflorian@outlook.fr>
2025-05-21 13:32:29 +0200

    21/05 étape 1.4

--- Backend/app.js
+++ Backend/app.js
@@ -29,5 +29,6 @@ app.get('/', (req, res) => {
 
 app.use('/api/books', booksRoutes);
 app.use('/api/auth', userRoutes);
+app.use('images', express.static(path.join(__dirname, 'images')));
 
 module.exports = app;
--- Backend/routes/books.js
+++ Backend/routes/books.js
@@ -4,10 +4,10 @@ const router = express.Router();
 const multer = require('../middleware/multer-config');
 const stuffCtrl = require('../controllers/books');
 
-router.post('/', multer, stuffCtrl.createBook);
-router.delete('/:id',stuffCtrl.deleteBook);
-router.put('/:id', stuffCtrl.updateBook);
+router.post('/', auth,multer, stuffCtrl.createBook);
+router.delete('/:id',auth,stuffCtrl.deleteBook);
+router.put('/:id',auth, multer, stuffCtrl.updateBook);
 router.get('/:id', stuffCtrl.getOneBook);
-router.get('/', stuffCtrl.getAllBooks);
+router.get('/',auth,stuffCtrl.getAllBooks);
 
 module.exports = router;
